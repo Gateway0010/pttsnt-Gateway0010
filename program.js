28d6ef4c292d953b37040b857f155d75c7450693
gian <gian.tt3046@sinhvien.hoasen.edu.vn>
2021-01-28 22:16:51 -0800

    nop bai

--- program.js
+++ program.js
@@ -1,8 +1,17 @@
 // Add new functions, variables here
-
+function tsnt(a){
+  for (let i=2;i<=a;i++){
+    while (a%i ==0){
+      console.log(i+" "+(a/=i));
+    }
+  }
+  return ;
+}
 function main(input) {
   // Your code goes here
+  let inp = input.split(" ")
+  let a = parseInt(inp[0]);
+  tsnt(a);
   // Using console.log to answer the question
 }
-
 module.exports = main;
